17adfc08490972405b413ec7b5082066e3328ac5
mikeoc61 <mikeoc61@me.com>
2026-05-24 10:54:10 -1000

    release: bump version to 1.2.0

--- src/seismic_reporting/__init__.py
+++ src/seismic_reporting/__init__.py
@@ -26,7 +26,7 @@ from seismic_reporting.core import (
 )
 from seismic_reporting.haversine import EARTH_RADIUS_KM, calc_dist
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 __author__ = "Michael E. O'Connor"
 
 __all__ = [
